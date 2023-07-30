40523d801364c1989a1674c750c29968d57fb3ad
Sena-Ok <ksenia1ok1scsfl@gmail.com>
2023-07-30 18:20:36 +0300

    reviews-3

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,21 +1,15 @@
-const swiper = new Swiper('.swiper', {
+var swiper = new Swiper('.swiper', {
     // Optional parameters
-    direction: 'vertical',
+    direction: 'horizontal',
     loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    freeMode: true,
   
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
     },
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
   });
