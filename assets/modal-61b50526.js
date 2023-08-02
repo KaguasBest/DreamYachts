2dde060a86561c17ae5e6af1882d7ab25002c5c2
KaguasBest <129059139+KaguasBest@users.noreply.github.com>
2023-08-02 12:46:36 +0000

    Deploying to gh-pages from @ KaguasBest/DreamYachts@64f3d5b28e5958d7e892dc1c541a41ed81a94d5a 🚀

---
+++ assets/modal-61b50526.js
@@ -0,0 +1,1 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o);function o(){e.modal.classList.toggle("is-hidden")}})();
