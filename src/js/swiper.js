5f417b616363a1525a51f4d6c18dc38aecaaf93f
vsevladimir <97120209+vsevladimir@users.noreply.github.com>
2023-08-04 03:32:20 +0300

    debuging yacht section
    added text styles to swiper items
    fixed text positioning
    added breakpoints to swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -17,8 +17,8 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
-  slidesPerView: 'auto',
-  // spaceBetween: 16,
+  // slidesPerView: 'auto',
+  // // spaceBetween: 16,
 
   // If we need pagination
   pagination: {
@@ -30,5 +30,27 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+
+  // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 16,
+
+  breakpoints: {
+    // when window width is >= 375px
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 16
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16
+    },
+    // when window width is >= 1280px
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 16
+    }
+  }
 
 });
